17fb6ac1c07149c05d7d0bd273fc35409726da1d
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-31 14:23:26 +0200

    chore(flake.nix): bump version

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
 
         packages.pricetag = pkgs.buildGoModule {
           pname = "pricetag";
-          version = "2026.05.31-a";
+          version = "2026.05.31-b";
 
           src = self;
 
