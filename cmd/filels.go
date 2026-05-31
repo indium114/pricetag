aa922a8f5c6f33f5444d5bd4f55b2eaeb5a43d7d
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-31 14:19:08 +0200

    feat: --grid mode for file ls

--- cmd/filels.go
+++ cmd/filels.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"strings"
 
 	"github.com/indium114/pricetag/internal"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 var showAll bool
+var gridMode bool
 
-// filelsCmd represents the filels command
+var ansiRegex = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+
+func displayWidth(s string) int {
+	plain := ansiRegex.ReplaceAllString(s, "")
+	return len([]rune(plain))
+}
+
+type renderedEntry struct {
+	display string
+	width   int
+}
+
 var filelsCmd = &cobra.Command{
 	Use:   "ls [directory]",
 	Short: "List the contents of a directory, including tags",
@@ -58,6 +72,8 @@ var filelsCmd = &cobra.Command{
 			return files[i].Name() < files[j].Name()
 		})
 
+		var renderedEntries []renderedEntry
+
 		// Directories
 		for _, d := range dirs {
 			name := d.Name()
@@ -86,11 +102,14 @@ var filelsCmd = &cobra.Command{
 				}
 			}
 
+			var disp string
 			if len(tagStrings) > 0 {
-				fmt.Println(colorBase + " " + strings.Join(tagStrings, " "))
+				disp = colorBase + " " + strings.Join(tagStrings, " ")
 			} else {
-				fmt.Println(colorBase)
+				disp = colorBase
 			}
+
+			renderedEntries = append(renderedEntries, renderedEntry{display: disp, width: displayWidth(disp)})
 		}
 
 		// Files
@@ -134,10 +153,55 @@ var filelsCmd = &cobra.Command{
 				}
 			}
 
+			var disp string
 			if len(tagStrings) > 0 {
-				fmt.Println(colorBase + " " + strings.Join(tagStrings, ""))
+				disp = colorBase + " " + strings.Join(tagStrings, "")
 			} else {
-				fmt.Println(colorBase)
+				disp = colorBase
+			}
+			renderedEntries = append(renderedEntries, renderedEntry{display: disp, width: displayWidth(disp)})
+		}
+
+		if gridMode {
+			maxWidth := 0
+			for _, e := range renderedEntries {
+				if e.width > maxWidth {
+					maxWidth = e.width
+				}
+			}
+
+			padding := 2
+			cellWidth := maxWidth + padding
+
+			termWidth := 80
+			if w, _, err := term.GetSize(int(os.Stdout.Fd())); err == nil {
+				termWidth = w
+			}
+
+			cols := termWidth / cellWidth
+			if cols < 1 {
+				cols = 1
+			}
+			rows := (len(renderedEntries) + cols - 1) / cols
+
+			for row := 0; row < rows; row++ {
+				for col := 0; col < cols; col++ {
+					idx := col*rows + row
+					if idx >= len(renderedEntries) {
+						continue
+					}
+					e := renderedEntries[idx]
+					if col < cols-1 {
+						fmt.Print(e.display + strings.Repeat(" ", cellWidth-e.width))
+					} else {
+						fmt.Print(e.display)
+					}
+				}
+				fmt.Println()
+			}
+		} else {
+			for _, e := range renderedEntries {
+				fmt.Println(e.display)
 			}
 		}
 
@@ -148,4 +212,5 @@ var filelsCmd = &cobra.Command{
 func init() {
 	fileCmd.AddCommand(filelsCmd)
 	filelsCmd.Flags().BoolVarP(&showAll, "all", "a", false, "Show hidden files")
+	filelsCmd.Flags().BoolVar(&gridMode, "grid", false, "Display in grid layout")
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
+	golang.org/x/term v0.43.0 // indirect
 )
